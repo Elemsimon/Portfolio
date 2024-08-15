2c09b52a82c28d0a8e49f0ba05816b38eae52785
Elemsimon <elemifesinachisimon@gmail.com>
2024-08-15 02:44:41 +0100

    update

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -4,6 +4,7 @@ import { FaLocationArrow } from "react-icons/fa6";
 import Link from 'next/link';
 
 import { projects } from "@/data";
+
 import { PinContainer } from "./ui/Pin";
 
 const RecentProjects = () => {
@@ -13,16 +14,20 @@ const RecentProjects = () => {
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
         {projects.map((item) => (
           <div
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+            className="sm:h-[41rem] h-[28rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[450px] lg:w-[480px] w-[80vw]"
             key={item.id}
           >
+            <PinContainer
+              title={item.link}
+              href={`/projects/${item.id}`}
+            >
             <Link href={`/projects/${item.id}`}>
-              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+              <div className="relative flex items-center justify-center sm:w-[500px] lg:w-[500px] w-[80vw] overflow-hidden sm:h-[45vh] lg:h-[40vh] h-[32vh] mb-10">
                 <div
-                  className="relative w-full h-full overflow-hidden lg:rounded-3xl"
+                  className="relative w-full h-full overflow-hidden lg:rounded-3xl "
                   style={{ backgroundColor: "#13162D" }}
                 >
                   <img src="/bg.png" alt="bgimg" />
@@ -30,7 +35,7 @@ const RecentProjects = () => {
                 <img
                   src={item.img}
                   alt="cover"
-                  className="z-10 absolute bottom-0"
+                  className="z-10 absolute -bottom-10 object-contain"
                 />
               </div>
 
@@ -49,7 +54,7 @@ const RecentProjects = () => {
               </p>
 
               <div className="flex items-center justify-between mt-7 mb-3">
-                <div className="flex items-center">
+                {/* <div className="flex items-center">
                   {item.iconLists.map((icon, index) => (
                     <div
                       key={index}
@@ -61,16 +66,17 @@ const RecentProjects = () => {
                       <img src={icon} alt="icon5" className="p-2" />
                     </div>
                   ))}
-                </div>
+                </div> */}
 
                 <div className="flex justify-center items-center">
                   <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    Check Live Site
+                     Read More
                   </p>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
               </div>
             </Link>
+            </PinContainer>
           </div>
         ))}
       </div>
