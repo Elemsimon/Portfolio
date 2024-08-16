6320003044c1d193e1eaab551846b627af37e036
Elemsimon <elemifesinachisimon@gmail.com>
2024-08-17 00:57:25 +0100

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,10 +2,11 @@ import { FaLocationArrow } from "react-icons/fa6";
 
 import { socialMedia } from "@/data";
 import MagicButton from "./MagicButton";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
+    <footer className="w-full pt-0 md:pt-20 pb-14" id="contact">
       {/* background grid */}
       {/* <div className="w-full absolute left-0 -bottom-5 min-h-96">
         <img
@@ -41,10 +42,10 @@ const Footer = () => {
           {socialMedia.map((info) => (
             <div
               key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              className="w-10 h-10 md:mt-0 mt-7  cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             > 
             <a href={info.link}>
-              <img src={info.img} alt="icons" width={20} height={20} />
+              <Image src={info.img} alt="icons" width={20} height={20} />
             </a>
               
             </div>
