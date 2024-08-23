f6874766d314f6f6d1d5505604cdd2645164b3d6
Elemsimon <elemifesinachisimon@gmail.com>
2024-08-23 18:49:32 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        <link rel="icon" href="/logo.png" sizes="any" className="w-full"/>
       </head>
       <body className={inter.className}>
         <ThemeProvider
